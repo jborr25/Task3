237c7ce5711b4e183ed0085534929d5660df9644
jborr25 <jovetborrico@gmail.com>
2024-06-23 19:04:48 +0000

    Update

--- Proj1.java
+++ Proj1.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class Proj1  {
+    public static void main(String[] args) {
+        
+        String n1 = "Hello";
+        String name;
+        Scanner console = new Scanner (System.in);
+        
+        System.out.println("Enter your name:");
+        name = console.next();
 
+        System.out.println(n1 + " " +name +":");
+
+    }
+}
